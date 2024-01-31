1a855c0820d5c296a346c1b6ed82933cffdd75bd
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-01-31 12:43:06 +0200

    updated my logo i mapped the different manufactures of cars by using headless Ui and tailwind CSS , I added new components that will show the different manufacturers we have as a rental car company

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,11 +8,11 @@ const Footer = () => (
     <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
       <div className="flex flex-col justify-start items-start gap-6">
         <Image
-          src="/logo.svg"
+          src="/zoom-fleet-rentals-logo.png"
           alt="logo"
           width={118}
           height={18}
-          className="object-contain"
+          className="object-contain rounded-full"
         />
         <p className="text-base text-gray-700">
           Zoom Fleet Rentals 2024 <br />
