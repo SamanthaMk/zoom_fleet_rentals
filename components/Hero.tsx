4ceada1cb37651e40c996bd8cbbbc94089ae1d83
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-02-03 05:52:34 +0200

    I updated my website by adding  new components, carCard and CarDetails these components display the different types of cars available to  rent the data was fetched from car ninja AP from Rapid API  page, I also added headless Ui to make drop box menus, tailwind CSS for styling the website

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {};
+ const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
